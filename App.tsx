5de69c2d2ce4629d0e1cf86cc681860e30ede0fa
Git-Yxt <62459204+Git-Yxt@users.noreply.github.com>
2022-09-20 19:55:00 +0800

    9.20

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
 import * as React from 'react';
 import './style.css';
-import { Button } from '@fluentui/react-northstar';
+import { Button, Popup, Slider } from '@fluentui/react-northstar';
+import { MoreIcon } from '@fluentui/react-icons-northstar';
 
 export default function App() {
   return (
     <div>
-      <Button>Hola</Button>
+      <Button content="Repository" tinted />
+      <Popup
+        content="Transmit interface"
+        trigger={<Button content="A trigger" />}
+      />
+      <Popup
+        trigger={<Button icon={<MoreIcon />} title="Show popup" />}
+        content="Hello from popup!"
+      />
+
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
 }
+// {/* <Popup>
+//         position="below" align="center" content="abc" on="click" trigger=
+//         {<Button content="Repository" />}
+//       </Popup> */}
